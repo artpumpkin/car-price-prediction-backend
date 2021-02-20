60c14e4e130525563dbafc692d67c2a2ee30167d
artpumpkin <pumpkin.salah@gmail.com>
2021-02-20 17:41:26 +0100

    fix: heroku

--- app/main.py
+++ app/main.py
@@ -2,8 +2,10 @@ from os.path import dirname, realpath, abspath, join
 import numpy as np
 import pickle
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__) 
+CORS(app)
 
 @app.route('/hello', methods=['GET'])
 def hello():
